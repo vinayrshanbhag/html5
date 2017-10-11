021bb293411e65a2a362cc48a85e0c48858b5972
vinayrshanbhag <vinay.shanbhag@live.com>
2017-10-11 10:30:24 +0530

    Usual commit

---
+++ Chapter4/geoLocation.js
@@ -0,0 +1,35 @@
+var getGeoLocation = function(elementToUpdate){
+    
+    var locationInfo = function(position){
+         var latitude = position.coords.latitude;
+         var longitude = position.coords.longitude;
+         var details = '';
+        
+        
+         var properties = ['latitude','longitude','altitude',
+                           'altitudeAccuracy','heading','speed'];
+        
+        for(var prop in position.coords){
+            details+=
+                "<p>" +prop+":" +position.coords[prop]+ "</p>"
+        }
+              
+       // elementToUpdate.innerHTML= "lat=" + latitude+ ", lng =" + longitude;
+       /* elementToUpdate.innerHTML = 
+            "<a href='http://maps.google.com?ll="+latitude +","+longitude+" ' target='_blank'> Your current location </a>";*/
+        
+        elementToUpdate.innerHTML= details;
+        
+     
+    }
+    var locationInfoError = function(error){
+        var errorMessage =['',
+                           'Permission denied',
+                           'Position Unavailable',
+                           'timeout'];
+        
+        alert('Error receiving location info' + errorMessage[error.code]);
+    }
+    
+    navigator.geolocation.getCurrentPosition(locationInfo,locationInfoError);
+}
